dca47b2d061827a444234cf946810ab831d5538b
DropD <r.haeuselmann@gmx.ch>
2017-01-12 11:33:55 +0100

    wrap code in function exposed via entry points

--- aiida_mul/code.py
+++ aiida_mul/code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-import json
-import sys
+def mul():
+    import json
+    import sys
 
-in_file = sys.argv[1]
-out_file = sys.argv[2]
+    in_file = sys.argv[1]
+    out_file = sys.argv[2]
 
-with open(in_file) as f:
-    in_dict = json.load(f)
+    with open(in_file) as f:
+        in_dict = json.load(f)
 
-out_dict = {'product': in_dict['x1'] * in_dict['x2']}
+    out_dict = {'product': in_dict['x1'] * in_dict['x2']}
 
-with open(out_file, 'w') as f:
-    json.dump(out_dict, f)
+    with open(out_file, 'w') as f:
+        json.dump(out_dict, f)
