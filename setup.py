3abc1c022771709ead0f0168e2f76e9ce5eccebb
DropD <r.haeuselmann@gmx.ch>
2017-02-20 15:38:54 +0100

    make almost fit for registry

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import json
 
 if __name__ == '__main__':
+    with open('setup.json', 'r') as info:
+        kwargs = json.load(info)
     setup(
-        name='aiida_mul',
-        author='Rico Haeuselmann',
-        author_email='haeuselm@epfl.ch',
-        classifiers=[
-            'Programming Language :: Python'
-        ],
-        version='0.1',
-        install_requires=[
-            'aiida >= 0.7'
-        ],
         packages=find_packages(),
-        entry_points={
-            'console_scripts': [
-                'aiida_mul = aiida_mul.code:mul'
-            ],
-            'aiida.calculations': [
-                'mul = aiida_mul.calcs:MultiplyCalculation'
-            ],
-            'aiida.parsers': [
-                'mul = aiida_mul.parsers:MultiplyParser'
-            ]
-        },
-        scripts=['bin/submit_mul']
+        **kwargs
     )
