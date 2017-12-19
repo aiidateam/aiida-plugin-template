6d7d44439c545935a99ef11f3cd1490620f2e6bb
Leopold Talirz <leopold.talirz@gmail.com>
2017-12-19 18:23:21 +0100

    fix data cmd

--- aiida_plugin_template/data/data_cli.py
+++ aiida_plugin_template/data/data_cli.py
@@ -1,5 +1,7 @@
 import sys
+import click
 from aiida.cmdline.commands import data_cmd
+from aiida.cmdline.dbenv_lazyloading import load_dbenv_if_not_loaded
 
 
 # See aiida.cmdline.data entry point in setup.json
@@ -14,9 +16,8 @@ def list_():  # pylint: disable=redefined-builtin
     """
     Display all MultiplyParameters nodes
     """
-    from aiida import is_dbenv_loaded, load_dbenv
-    if not is_dbenv_loaded():
-        load_dbenv()
+    load_dbenv_if_not_loaded(
+    )  # Important to load the dbenv in the last moment
 
     from aiida.orm.querybuilder import QueryBuilder
     from aiida.orm import DataFactory
@@ -31,3 +32,26 @@ def list_():  # pylint: disable=redefined-builtin
         obj = result[0]
         s += "{}, pk: {}\n".format(str(obj), obj.pk)
     sys.stdout.write(s)
+
+
+@cli.command('export')
+@click.option(
+    '--outfile',
+    '-o',
+    type=click.Path(dir_okay=False),
+    help='Write output to file (default: print to stdout).')
+@click.argument('pk', type=int)
+def export(outfile, pk):
+    """Export a MultiplyParameters node, identified by PK, to plain text"""
+    load_dbenv_if_not_loaded(
+    )  # Important to load the dbenv in the last moment
+
+    from aiida.orm import load_node
+    node = load_node(pk)
+    string = str(node)
+
+    if outfile:
+        with open(outfile, 'w') as f:
+            f.write(string)
+    else:
+        click.echo(string)
